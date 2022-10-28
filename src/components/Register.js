69d17de32e95601291c68a88c6b1ad85bec4b89f
dvo1 <duosinmivictory1@gmail.com>
2022-10-28 18:29:05 +0100

    Register.js uncommented

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,50 +1,50 @@
-// import React from 'react'
-// import { useState } from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import {signUpUser} from '../features/login/loginSlice'
+import React from 'react'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {signUpUser} from '../features/login/loginSlice'
 
-// const Register = () => {
-//     const [name, setName] = useState('')
-//     const [email, setEmail] = useState('')
-//     const [password, setPassword] = useState('')
+const Register = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-//     const dispatch =useDispatch()
+    const dispatch =useDispatch()
 
-//     const nameHandle = (e) => {
-//             setName(e.target.value)
-//     }
-//     const emailHandle = (e) => {
-//             setEmail(e.target.value)
-//     }
-//     const passwordHandle = (e) => {
-//             setPassword(e.target.value)
-//     }
+    const nameHandle = (e) => {
+            setName(e.target.value)
+    }
+    const emailHandle = (e) => {
+            setEmail(e.target.value)
+    }
+    const passwordHandle = (e) => {
+            setPassword(e.target.value)
+    }
 
-//     const registerHandle = (e) => {
-//         e.preventDefault()
-//         // console.log(name,email,password)
-//         dispatch(signUpUser({name, email, password}))
-//     }
+    const registerHandle = (e) => {
+        e.preventDefault()
+        // console.log(name,email,password)
+        dispatch(signUpUser({name, email, password}))
+    }
 
-//     return (
-//         <div>
-//             <div className="register-container">
-//                 <h3>Register</h3>
-//                 <div className='register-form'>
+    return (
+        <div>
+            <div className="register-container">
+                <h3>Register</h3>
+                <div className='register-form'>
 
-//                     <label htmlFor="">Name</label>
-//                     <input type="text" placeholder='Name' value={name} onChange={nameHandle} />
-//                     <label htmlFor="">Email</label>
-//                     <input type="email" placeholder='Email' value={email} onChange={emailHandle} />
-//                     <label htmlFor="">Password</label>
-//                     <input type="password" placeholder='Password' value={password} onChange={passwordHandle}/>
-//                     <button className='sub-btn' onClick={registerHandle}>Register</button>
+                    <label htmlFor="">Name</label>
+                    <input type="text" placeholder='Name' value={name} onChange={nameHandle} />
+                    <label htmlFor="">Email</label>
+                    <input type="email" placeholder='Email' value={email} onChange={emailHandle} />
+                    <label htmlFor="">Password</label>
+                    <input type="password" placeholder='Password' value={password} onChange={passwordHandle}/>
+                    <button className='sub-btn' onClick={registerHandle}>Register</button>
 
-//                 </div>
-//             </div>
+                </div>
+            </div>
 
-//         </div>
-//     )
-// }
+        </div>
+    )
+}
 
-// export default Register
+export default Register
